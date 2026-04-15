c6b3275aa6bc5afa555e928f263b71a2d9d904f0
season <706350462@qq.com>
2026-04-15 13:11:51 +0800

    langchain初始化方式 一.Model Class 二.init_chat_model

--- models/quick_start.py
+++ models/quick_start.py
@@ -1,7 +1,7 @@
 from langchain.agents import create_agent
 from langchain_core.tools import tool
 
-from my_llm import ollama_llm
+from my_llm import ollama_llm, ollama_llm_two
 
 """创建agent，调用工具回答用户问题"""
 
@@ -27,7 +27,17 @@ print(agent)
 resp = agent.invoke(
     {"messages": [{"role": "user", "content": "查询北京的天气"}]}
 )
-
 print(resp)
 
-print("hello")
+# 创建Agent
+agent_two = create_agent(
+    model=ollama_llm_two,
+    tools=[get_weather],
+    system_prompt="你是一个助手，你可以查询城市的天气。"
+)
+resp_two = agent_two.invoke(
+    {"messages": [{"role": "user", "content": "查询北京的天气"}]}
+)
+print(resp_two)
+
+
