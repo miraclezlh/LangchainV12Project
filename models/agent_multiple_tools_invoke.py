0be0d5a94bcc96a085dccd21e3f9d082d404b950
season <706350462@qq.com>
2026-04-17 14:37:27 +0800

    上下文记忆体，跟踪

--- models/agent_multiple_tools_invoke.py
+++ models/agent_multiple_tools_invoke.py
@@ -1,5 +1,4 @@
 from langchain.agents import create_agent
-from langchain_core.messages import HumanMessage
 
 from llm_init import ollama_llm_two
 from tools.common_tools import search_news, get_stock_price
@@ -11,8 +10,9 @@ agent = create_agent(
     system_prompt="你是一个助手，你可以查询公司的股价，以及有关公司的新闻搜索。"
 )
 
-# human_message = HumanMessage(content="苹果公司今天的股价是多少？最近有什么新闻？")
-response = agent.invoke( {"messages": [{"role": "user", "content": "苹果公司今天的股价是多少？最近有什么新闻？"}]})
-response = agent.invoke(human_message)
+# agent调用模型，必须是mesages的结构体作为入参
+response = agent.invoke({"messages": [{"role": "user", "content": "苹果公司今天的股价是多少？最近有什么新闻？"}]})
 print(type(response))
 print(response)
+print(response["messages"][-1].content)
+
--- models/multiple_tools_manual_invoke.py
+++ models/multiple_tools_manual_invoke.py
@@ -7,25 +7,34 @@ from tools.common_tools import get_stock_price, search_news
 tools = [get_stock_price, search_news]
 model_with_multiple_tools = ollama_llm_two.bind_tools(tools)
 
-message = []
+message = []    # 手搓上下文记忆列表
 human_message = HumanMessage(content="苹果公司今天的股价是多少？最近有什么新闻？")
 # human_message = HumanMessage(content="比较一下微软和苹果的股价")
 # human_message = HumanMessage(content="腾讯最近有什么重大新闻？")
 # human_message = HumanMessage(content="海水为什么是咸的？")
 message.append(human_message)
-
+# 第一次只包含：用户的问题HumanMessage
 print(message)
+print("==================")
 
-# 2. 工具调用
+count = 0
+# 2. 工具调用,迭代循环
 while True:
-    response = model_with_multiple_tools.invoke(message)
+    count += 1
 
+    response = model_with_multiple_tools.invoke(message)
+    # 第二次包含：用户的问题HumanMessage + AI返回的结果AIMessage（包含待调用的工具列表）
     message.append(response)
 
     print(message)
+    print("==================")
 
-    # print(response)
-    # print(response.tool_calls)
+    print(type(response))
+    print("response:",response)     # 打印返回的AIMessage的信息
+    print(response.tool_calls)  # 打印返回的Tool工具列表
+    print("==================")
+
+
     # 如果有调用工具，处理工具调用响应
     # 3.开发者根据模型的响应，调用工具并获取结果
     if response.tool_calls:
@@ -42,5 +51,6 @@ while True:
         print("没有工具调用，直接返回答案")
         break
 
-print("response", response)
+print("=====================")
+print("response:", response)
 print(response.content)
