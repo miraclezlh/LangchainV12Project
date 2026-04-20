de512e4f52adeca3075dcb4d5db7d513efc828f0
season <706350462@qq.com>
2026-04-20 16:02:56 +0800

    @wrap_model_call 动态切换大模型

--- agents/agent_wrap_call.py
+++ agents/agent_wrap_call.py
@@ -1,23 +1,27 @@
 from langchain.agents import create_agent
 from langchain.agents.middleware import ModelRequest, ModelResponse, wrap_model_call
 
-from llm_init import ollama_llm_qwen, ollama_llm_gemma
+from llm_init import ollama_llm_qwen, ollama_llm_qwen3_4b
 from tools.common_tools import search_news, get_stock_price
 
 
-# 调用工具前后执行，钩子函数
+# 调用大模型之前执行，钩子函数。动态调用，进行附属操作
 @wrap_model_call
 def dynamic_model_selection(request: ModelRequest, handler) -> ModelResponse:
     """动态修改模型"""
-    print(type(request))
-    print(request)
+    # print(type(request)) # 类型为：ModelRequest对象
+    # print(request)
 
     message_count = len(request.state["messages"])
     if message_count > 2:
-        request.override(model=ollama_llm_gemma)
+        print("切换大模型为ollama_llm_qwen3_4b")
+        model = ollama_llm_qwen3_4b
+        # request.override(model=ollama_llm_gemma)
     else:
-        request.override(model=ollama_llm_qwen)
-    return handler(request)
+        print("切换大模型为ollama_llm_qwen")
+        model = ollama_llm_qwen
+        # request.override(model=ollama_llm_qwen)
+    return handler(request.override(model=model))
 
 
 # 创建Agent
@@ -30,6 +34,6 @@ agent = create_agent(
 
 # agent调用模型，必须是mesages的结构体作为入参
 response = agent.invoke({"messages": [{"role": "user", "content": "苹果公司今天的股价是多少？最近有什么新闻？"}]})
-print(type(response))
+print(type(response)) # agent调用大模型，返回类型为dict,<class 'dict'>
 print(response)
 print(response["messages"][-1].content)
