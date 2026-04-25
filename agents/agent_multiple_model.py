e7f7030faf5abc08c2fe97a51b7de7a8142a0de8
season <706350462@qq.com>
2026-04-25 19:16:43 +0800

    提示词工程，初版示例

---
+++ agents/agent_multiple_model.py
@@ -0,0 +1,44 @@
+from langchain.agents import create_agent
+from langchain.agents.middleware import ModelRequest, ModelResponse, wrap_model_call
+from langchain_core.messages import HumanMessage
+
+from llm_init import ollama_llm_qwen, ollama_llm_qwen3_4b
+from tools.common_tools import search_news, get_stock_price
+
+
+# 调用大模型之前执行，钩子函数。动态调用，进行附属操作
+@wrap_model_call
+def dynamic_model_selection(request: ModelRequest, handler) -> ModelResponse:
+    """动态修改模型"""
+    # print(type(request)) # 类型为：ModelRequest对象
+    # print(request)
+
+    message_count = len(request.state["messages"])
+    if message_count > 2:
+        print("切换大模型为ollama_llm_qwen3_4b")
+        model = ollama_llm_qwen3_4b
+        # request.override(model=ollama_llm_gemma)
+    else:
+        print("切换大模型为ollama_llm_qwen")
+        model = ollama_llm_qwen
+        # request.override(model=ollama_llm_qwen)
+    return handler(request.override(model=model))
+
+
+# 创建Agent
+agent = create_agent(
+    model=ollama_llm_qwen,
+    tools=[get_stock_price, search_news],
+    # middleware=[dynamic_model_selection],
+    system_prompt="你是一个图片识别助手，请你详细描述图片资源。"
+)
+
+# agent调用模型，必须是mesages的结构体作为入参
+# 1.json格式
+resp_stream = agent.stream(HumanMessage([
+         {"type":"text","content":"描述以下图片的内容"},
+         {"type":"image","url":"https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png"},
+        ]))
+for token in resp_stream:
+    if token.content:
+        print(token.content, end="", flush=True)
---
+++ prompts/agent_with_prompt.py
@@ -0,0 +1,51 @@
+from langchain.agents import create_agent
+from langchain.agents.middleware import ModelRequest, ModelResponse, wrap_model_call
+
+from llm_init import ollama_llm_qwen, ollama_llm_qwen3_4b
+from tools.common_tools import search_news, get_stock_price
+
+
+# 调用大模型之前执行，钩子函数。动态调用，进行附属操作
+@wrap_model_call
+def dynamic_model_selection(request: ModelRequest, handler) -> ModelResponse:
+    """动态修改模型"""
+    # print(type(request)) # 类型为：ModelRequest对象
+    # print(request)
+
+    message_count = len(request.state["messages"])
+    if message_count > 2:
+        print("切换大模型为ollama_llm_qwen3_4b")
+        model = ollama_llm_qwen3_4b
+        # request.override(model=ollama_llm_gemma)
+    else:
+        print("切换大模型为ollama_llm_qwen")
+        model = ollama_llm_qwen
+        # request.override(model=ollama_llm_qwen)
+    return handler(request.override(model=model))
+
+# """ 多行字符串，或者文档字符串"""
+system_prompt="""
+你以海盗的口吻，来回答用户问题
+"""
+
+# 创建Agent
+agent = create_agent(
+    model=ollama_llm_qwen,
+    tools=[get_stock_price, search_news],
+    # middleware=[dynamic_model_selection],
+    system_prompt="你以海盗的口吻，来回答用户问题"
+)
+
+# agent调用模型，必须是mesages的结构体作为入参
+# 流式调用
+resp_stream = agent.stream(
+    {"messages": [{"role": "user", "content": "你是谁？"}]},
+    stream_mode="messages"
+)
+print(type(resp_stream))  # agent调用大模型，stream的返回类型为<class 'generator'>
+
+for token, metadata in resp_stream:
+    if token.content:
+        print(token.content, end="", flush=True)
+# print(response)
+# print(response["messages"][-1].content)
