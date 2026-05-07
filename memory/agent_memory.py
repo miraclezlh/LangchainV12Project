983eb9ebd5016ce196d16a21f0bccec2a1d3c8c0
season <706350462@qq.com>
2026-05-07 19:21:03 +0800

    短期记忆存放在DB

--- memory/agent_memory.py
+++ memory/agent_memory.py
@@ -1,26 +1,33 @@
 from langchain.agents import create_agent
-from langgraph.checkpoint.base import BaseCheckpointSaver
 from langgraph.checkpoint.memory import InMemorySaver
 
 from llm_init import ollama_llm_qwen
 
+"""
+一次会话：包含多次对话，HumanMessage
+checkpointer可以实现短期记忆
+
+
+
+"""
 # 创建Agent
 agent = create_agent(
     model=ollama_llm_qwen,
     tools=[],
     # system_prompt="你是一个助手，你可以查询公司的股价，以及有关公司的新闻搜索。",
+    # checkpionter是把过往的交互过程，保存在内存，或者DB，Redis
     checkpointer=InMemorySaver(),
 )
 
-config = {"configurable":{"thread_id":"session_1"}}
+config = {"configurable": {"thread_id": "session_1"}}
 
 # agent调用模型，必须是mesages的结构体作为入参
-response = agent.invoke({"messages": [{"role": "user", "content": "我叫小飞侠，你是谁？"}]},config=config)
+response = agent.invoke({"messages": [{"role": "user", "content": "我叫小飞侠，你是谁？"}]}, config=config)
 print(type(response))
 print(response)
 print(response["messages"][-1].content)
 
 print("================")
 
-response_two = agent.invoke({"messages": [{"role": "user", "content": "我叫什么名字？"}]},config=config)
+response_two = agent.invoke({"messages": [{"role": "user", "content": "我叫什么名字？"}]}, config=config)
 print(response_two["messages"][-1].content)
---
+++ memory/agent_memory_db.py
@@ -0,0 +1,43 @@
+from langchain.agents import create_agent
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.mysql.pymysql import PyMySQLSaver
+
+from llm_init import ollama_llm_qwen
+
+"""
+一次会话：包含多次对话，HumanMessage
+checkpointer可以实现短期记忆
+
+"""
+
+DB_URI = "mysql+pymysql://root:111111@localhost:3306/langchain_db?useSSL=false&useUnicode=true&characterEncoding=utf8mb4"
+
+"""
+with 等于 try......catch......finally
+"""
+with PyMySQLSaver.from_conn_string(DB_URI) as checkpointer:
+    """
+    自动创建表
+    """
+    checkpointer.setup()
+
+    # 创建Agent
+    agent = create_agent(
+        model=ollama_llm_qwen,
+        tools=[],
+        # checkpionter是把过往的交互过程，保存在内存，或者DB，Redis
+        checkpointer=checkpointer,
+    )
+
+    config = {"configurable": {"thread_id": "session_1"}}
+
+    # agent调用模型，必须是mesages的结构体作为入参
+    response = agent.invoke({"messages": [{"role": "user", "content": "我叫小飞侠，你是谁？"}]}, config=config)
+    print(type(response))
+    print(response)
+    print(response["messages"][-1].content)
+
+    print("================")
+
+    response_two = agent.invoke({"messages": [{"role": "user", "content": "我叫什么名字？"}]}, config=config)
+    print(response_two["messages"][-1].content)
